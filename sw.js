71e05718c78193cc2318c612c9bfba4a6bc53606
muxablya <ihome.mix@gmail.com>
2026-05-31 01:27:29 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'vfm-v1';
+const urlsToCache = ['/', '/index.php', '/static.mp3'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
